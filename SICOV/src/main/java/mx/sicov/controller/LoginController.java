4a66ce1bf2fcd5627df95f51b169b1aaf675e9a4
Daniel Gonzalez <20193tn108@utez.edu.mx>
2022-04-04 23:42:04 -0500

    Login messages

--- SICOV/src/main/java/mx/sicov/controller/LoginController.java
+++ SICOV/src/main/java/mx/sicov/controller/LoginController.java
@@ -1,15 +1,34 @@
 package mx.sicov.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping(value = {"/"})
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(){
+    public String login(@RequestParam(value = "error", required = false) String error, Principal principal,
+                        @RequestParam(value = "logout", required = false) String logout, Model model){
+        model.addAttribute("message","");
+        model.addAttribute("alert","");
+        if (error != null){
+            model.addAttribute("message","Error al iniciar sesión");
+            model.addAttribute("alert","error");
+        }
+        if(logout != null){
+            model.addAttribute("message","Sesión cerrada");
+            model.addAttribute("alert","success");
+        }
+        if (principal != null){
+            model.addAttribute("message","Ya tienes una sesión iniciada");
+            model.addAttribute("alert","warning");
+        }
         return "login";
     }
 
