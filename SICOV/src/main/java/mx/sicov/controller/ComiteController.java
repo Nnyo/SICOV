1d969fe50321cb9a1f294c3fdf0510afdab358d4
Jean Hernandez <jeanhernandez@utez.edu.mx>
2022-04-14 00:50:31 -0500

    Getters and Setters

--- SICOV/src/main/java/mx/sicov/controller/ComiteController.java
+++ SICOV/src/main/java/mx/sicov/controller/ComiteController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
 @RequestMapping(value = {"/comite"})
 public class ComiteController {
--- SICOV/src/main/java/mx/sicov/entity/Categoria.java
+++ SICOV/src/main/java/mx/sicov/entity/Categoria.java
@@ -23,4 +23,33 @@ public class Categoria {
     public Categoria() {
     }
 
+    public Categoria(Long idcategoria,
+            @NotBlank(message = "El nombre es requerido") @Size(max = 120, message = "El nombre no puede tener más de 120 caracteres") @Size(min = 3, message = "El nombre no puede ser menor de 3 caracteres") String nombre) {
+        this.idcategoria = idcategoria;
+        this.nombre = nombre;
+    }
+
+    public Long getIdcategoria() {
+        return idcategoria;
+    }
+
+    public void setIdcategoria(Long idcategoria) {
+        this.idcategoria = idcategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria [idcategoria=" + idcategoria + ", nombre=" + nombre + "]";
+    }
+
+    
+
 }
