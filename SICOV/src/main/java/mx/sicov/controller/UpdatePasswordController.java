0fcd55687893e1957b73dcf457d916769a31ea5f
Daniel Gonzalez <20193tn108@utez.edu.mx>
2022-04-15 18:23:02 -0500

    Authentication UpdatePasswordController

--- SICOV/src/main/java/mx/sicov/controller/UpdatePasswordController.java
+++ SICOV/src/main/java/mx/sicov/controller/UpdatePasswordController.java
@@ -1,6 +1,8 @@
 package mx.sicov.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UpdatePasswordController {
 
     @GetMapping("/update")
-    public String modificarPassword(){
+    public String modificarPassword(Authentication authentication, Model model){
+        model.addAttribute("role",authentication.getAuthorities().toString());
         return "updatePassword";
     }
 }
