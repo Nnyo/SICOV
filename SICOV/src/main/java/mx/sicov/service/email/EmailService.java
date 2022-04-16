edc6c1f849c11a4c91b4871a06c2f85f3d4b7232
Elizabeth Estrada <20193tn138@utez.edu.mx>
2022-04-16 15:55:26 -0500

    Correo

---
+++ SICOV/src/main/java/mx/sicov/service/email/EmailService.java
@@ -0,0 +1,24 @@
+package mx.sicov.service.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+   
+    public void sendSimpleMail(String email, String encabezado, String mensaje){
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("20193tn138@utez.edu.mx");
+            message.setTo(email);
+            message.setSubject(encabezado);
+            message.setText(mensaje);
+            mailSender.send(message);
+            
+            System.out.println("Mail sent successfully...");
+        }
+}
